18d23d16ee1954e0a72e1c42ef0537860b159d51
zhaiwei <zhaiwei@qq.com>
2016-07-06 16:05:21 +0800

    Hello guys! We made it!

--- server/src/main/java/cn/edu/nju/ServerApplication.java
+++ server/src/main/java/cn/edu/nju/ServerApplication.java
@@ -2,6 +2,9 @@ package cn.edu.nju;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -9,4 +12,15 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+
+	@RestController
+	public static class MyRestController {
+
+		@RequestMapping(value = "/",method = RequestMethod.GET)
+		public String hello(){
+			return "Hello guys! We made it!";
+		}
+
+	}
+
 }
