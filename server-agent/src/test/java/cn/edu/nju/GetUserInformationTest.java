ebf3c97621bae62ac2a714df1bb1927711b1c743
chezeyu <897469639@qq.com>
2016-07-18 20:55:10 +0800

    GetUserInformationTest和GetUserListTest

---
+++ server-agent/src/test/java/cn/edu/nju/GetUserInformationTest.java
@@ -0,0 +1,71 @@
+package cn.edu.nju;
+
+import cn.edu.nju.datatables.User;
+import com.google.gson.Gson;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import static cn.edu.nju.GlobalVar.host;
+
+/**
+ * Created by chezeyu on 2016/7/18.
+ */
+public class GetUserInformationTest {
+
+    /**
+     * GetUserInformationTest:查询用户所有可查询个人信息
+     */
+    private static String url = "http://"+host+"/users/";
+
+    public static void testModel(long id, String name,String phone,int type){
+        /**
+         * testModel(test模板：验证通过id查询单个人信息，id存在)
+         * 输入：id = id
+         * 预计输出：id = id，name = name，phone = phone，type = type
+         */
+
+        Gson gson = new Gson();
+
+        /**预计结果*/
+        User user = new User(id, name, null, phone, type);
+        String expectedInformation = gson.toJson(user,User.class);
+
+        /**发送请求*/
+        String returnInformation = HttpRequest.sendGet(url+id,"");
+
+        /**检查返回值跟预期值是否一致*/
+        assertEquals(expectedInformation,returnInformation);
+    }
+
+    @Test
+    public void test01(){
+        /**
+         * test01(验证通过id查询单个人信息，id存在)
+         * 输入：id = {1,101,201,301}
+         * 预计输出：id = 1，  name = admin，      phone = 001，type = 0；
+         *           id = 101，name = engineer，   phone = 101，type = 1；
+         *           id = 201，name = saler，      phone = 201，type = 2；
+         *           id = 301，name = distributor，phone = 301，type = 3；
+         */
+        testModel(1,"admin","001",0);
+        testModel(101,"engineer","101",1);
+        testModel(201,"saler","201",2);
+        testModel(301,"distributor","301",3);
+    }
+
+    @Test
+    public void test02(){
+        /**
+         * test02(验证通过id查询单个人信息，id不存在))
+         * 输入：id = 0
+         * 预计输出：
+         */
+
+        /**发送请求*/
+        String returnInformation = HttpRequest.sendGet(url+0,"");
+
+        /**检查返回值跟预期值是否一致*/
+
+    }
+}
---
+++ server-agent/src/test/java/cn/edu/nju/model.java
@@ -0,0 +1,25 @@
+package cn.edu.nju;
+
+import org.junit.Test;
+
+import static cn.edu.nju.GlobalVar.host;
+
+/**
+ * Created by chezeyu on 2016/7/18.
+ */
+public class model {
+
+    /**
+     * GetUserInformationTest:查询用户所有可查询个人信息
+     */
+    private static String url = "http://"+host+"/users/";
+
+    @Test
+    public void test01(){
+        /**
+         * test01(验证)
+         * 输入：
+         * 预计输出：
+         */
+    }
+}
