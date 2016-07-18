e15cfa461045d1c5ce5a4dfc499ece5bcd2ba6aa
chezeyu <897469639@qq.com>
2016-07-18 20:18:02 +0800

    LoginTest改用Gson去格式化数据

--- server-agent/src/test/java/cn/edu/nju/LoginTest.java
+++ server-agent/src/test/java/cn/edu/nju/LoginTest.java
@@ -3,8 +3,8 @@ package cn.edu.nju;
 import cn.edu.nju.servicedata.users.LoginRequest;
 
 import cn.edu.nju.servicedata.users.LoginResponse;
+import com.google.gson.Gson;
 import org.junit.Test;
-import net.sf.json.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,6 +13,7 @@ import static cn.edu.nju.GlobalVar.host;
 /**
  * Created by chezeyu on 2016/7/18.
  */
+
 public class LoginTest {
 
     /**
@@ -34,27 +35,30 @@ public class LoginTest {
     @Test
     public void test02(){
         /**
-         * test02(验证存在用户登录正常)
+         * test02(验证存在用户登录正常，管理员账号)
          * 输入：id = 1，password = admin
          * 预计输出：success = true，info = null，type = 0
          */
+
+        Gson gson = new Gson();
 
         /**待发送信息*/
         LoginRequest request = new LoginRequest();
         request.setId(1);
         request.setPassword("admin");
-        JSONObject postInformation = JSONObject.fromObject(request);
+        String postInformation = gson.toJson(request);
 
-        //！！！！！！！！！！！！！！！！！！调换默认构造顺序
         /**预计结果*/
         LoginResponse response = new LoginResponse(true,0,null);
-        JSONObject expactedInformation = JSONObject.fromObject(response);
+        String expectedInformation = gson.toJson(response);
 
         /**发送请求*/
-        String ret = HttpRequest.sendPost(url+"/login",postInformation.toString());
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
 
         /**检查返回值跟预期值是否一致*/
-        assertEquals(expactedInformation,ret);
+        assertEquals(expectedInformation,returnInformation);
     }
 
     @Test
@@ -65,22 +69,26 @@ public class LoginTest {
          * 预计输出：success = false，info = "The user do not exists."，type = -1
          */
 
+        Gson gson = new Gson();
+
         /**待发送信息*/
         LoginRequest request = new LoginRequest();
         request.setId(99999);
         request.setPassword("admin");
-        JSONObject postInformation = JSONObject.fromObject(request);
+        String postInformation = gson.toJson(request);
 
         /**预计结果*/
         String info = "The user do not exists.";
-        LoginResponse response = new LoginResponse(false, -1, info);
-        JSONObject expactedInformation = JSONObject.fromObject(response);
+        LoginResponse response = new LoginResponse(false,-1,info);
+        String expectedInformation = gson.toJson(response);
 
         /**发送请求*/
-        String ret = HttpRequest.sendPost(url + "/login", postInformation.toString());
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
 
         /**检查返回值跟预期值是否一致*/
-        assertEquals(expactedInformation, ret);
+        assertEquals(expectedInformation,returnInformation);
     }
 
     @Test
@@ -91,22 +99,26 @@ public class LoginTest {
          * 预计输出：success = false，info = "Wrong password."，type = -1
          */
 
+        Gson gson = new Gson();
+
         /**待发送信息*/
         LoginRequest request = new LoginRequest();
         request.setId(1);
         request.setPassword("add");
-        JSONObject postInformation = JSONObject.fromObject(request);
+        String postInformation = gson.toJson(request);
 
         /**预计结果*/
-        String info = "Wrong password.";
+        String info ="Wrong password.";
         LoginResponse response = new LoginResponse(false,-1,info);
-        JSONObject expactedInformation = JSONObject.fromObject(response);
+        String expectedInformation = gson.toJson(response);
 
         /**发送请求*/
-        String ret = HttpRequest.sendPost(url+"/login",postInformation.toString());
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
 
         /**检查返回值跟预期值是否一致*/
-        assertEquals(expactedInformation,ret);
+        assertEquals(expectedInformation,returnInformation);
     }
 
     @Test
@@ -117,45 +129,142 @@ public class LoginTest {
          * 预计输出：success = false，info = "The user do not exists."，type = -1
          */
 
+        Gson gson = new Gson();
+
         /**待发送信息*/
         LoginRequest request = new LoginRequest();
         request.setPassword("admin");
-        JSONObject postInformation = JSONObject.fromObject(request);
+        String postInformation = gson.toJson(request);
 
         /**预计结果*/
         String info = "The user do not exists.";
         LoginResponse response = new LoginResponse(false,-1,info);
-        JSONObject expactedInformation = JSONObject.fromObject(response);
+        String expectedInformation = gson.toJson(response);
 
         /**发送请求*/
-        String ret = HttpRequest.sendPost(url+"/login",postInformation.toString());
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
 
         /**检查返回值跟预期值是否一致*/
-        assertEquals(expactedInformation,ret);
+        assertEquals(expectedInformation,returnInformation);
     }
 
     @Test
     public void test06(){
         /**
          * test06(验证密码为空时登录情况)
-         * 输入：id = 1，password = null
+         * 输入：id = 1，password = ""
          * 预计输出：success = false，info = "Wrong password."，type = -1
          */
+
+        Gson gson = new Gson();
 
         /**待发送信息*/
         LoginRequest request = new LoginRequest();
         request.setId(1);
-        JSONObject postInformation = JSONObject.fromObject(request);
+        request.setPassword("");
+        String postInformation = gson.toJson(request);
 
         /**预计结果*/
         String info = "Wrong password.";
         LoginResponse response = new LoginResponse(false,-1,info);
-        JSONObject expactedInformation = JSONObject.fromObject(response);
+        String expectedInformation = gson.toJson(response);
 
         /**发送请求*/
-        String ret = HttpRequest.sendPost(url+"/login",postInformation.toString());
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
 
         /**检查返回值跟预期值是否一致*/
-        assertEquals(expactedInformation,ret);
+        assertEquals(expectedInformation,returnInformation);
+    }
+
+    @Test
+    public void test07(){
+        /**
+         * test07(验证存在用户登录正常，工程师账号)
+         * 输入：id = 101，password = engineer
+         * 预计输出：success = true，info = null，type = 1
+         */
+
+        Gson gson = new Gson();
+
+        /**待发送信息*/
+        LoginRequest request = new LoginRequest();
+        request.setId(101);
+        request.setPassword("engineer");
+        String postInformation = gson.toJson(request);
+
+        /**预计结果*/
+        LoginResponse response = new LoginResponse(true,1,null);
+        String expectedInformation = gson.toJson(response);
+
+        /**发送请求*/
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
+
+        /**检查返回值跟预期值是否一致*/
+        assertEquals(expectedInformation,returnInformation);
+    }
+
+    @Test
+    public void test08(){
+        /**
+         * test08(验证存在用户登录正常，销售员账号)
+         * 输入：id = 201，password = saler
+         * 预计输出：success = true，info = null，type = 2
+         */
+
+        Gson gson = new Gson();
+
+        /**待发送信息*/
+        LoginRequest request = new LoginRequest();
+        request.setId(201);
+        request.setPassword("saler");
+        String postInformation = gson.toJson(request);
+
+        /**预计结果*/
+        LoginResponse response = new LoginResponse(true,2,null);
+        String expectedInformation = gson.toJson(response);
+
+        /**发送请求*/
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
+
+        /**检查返回值跟预期值是否一致*/
+        assertEquals(expectedInformation,returnInformation);
+    }
+
+    @Test
+    public void test09(){
+        /**
+         * test09(验证存在用户登录正常，派单员账号)
+         * 输入：id = 301，password = distributor
+         * 预计输出：success = true，info = null，type = 3
+         */
+
+        /**待发送信息*/
+        Gson gson = new Gson();
+
+        /**待发送信息*/
+        LoginRequest request = new LoginRequest();
+        request.setId(301);
+        request.setPassword("distributor");
+        String postInformation = gson.toJson(request);
+
+        /**预计结果*/
+        LoginResponse response = new LoginResponse(true,3,null);
+        String expectedInformation = gson.toJson(response);
+
+        /**发送请求*/
+        String returnInformation = HttpRequest.sendPost(url+"/login",postInformation);
+        LoginResponse temp = gson.fromJson(returnInformation,LoginResponse.class);
+        returnInformation = gson.toJson(temp);
+
+        /**检查返回值跟预期值是否一致*/
+        assertEquals(expectedInformation,returnInformation);
     }
 }
